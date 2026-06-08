13906c5dd729313753dec5c029c7d2410a08d567
Larissa Bairon <larissabairon2020@gmail.com>
2026-06-08 20:59:22 -0300

    feat: adicionar rota PATCH para cancelar ordem de compra

--- src/routes/pedidoRoutes.js
+++ src/routes/pedidoRoutes.js
@@ -8,11 +8,14 @@ router.get('/', pedidoController.getAll.bind(pedidoController));
 // GET pedido por ID
 router.get('/:id', pedidoController.getById.bind(pedidoController));
 
-// POST criar pedido
+// POST criar pedido / receber pedido de compra
 router.post('/', pedidoController.create.bind(pedidoController));
 
 // PUT atualizar pedido
 router.put('/:id', pedidoController.update.bind(pedidoController));
+
+// PATCH cancelar pedido de compra
+router.patch('/:id/cancelar', pedidoController.cancelar.bind(pedidoController));
 
 // DELETE remover pedido
 router.delete('/:id', pedidoController.delete.bind(pedidoController));
